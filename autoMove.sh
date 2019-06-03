7d936ef6139aeb45b912cdd347e3d18aabb4e4b6
Bryan Serrano <bserran2@illinois.edu>
2019-06-02 21:20:08 -0500

    added ability to send to other machine

--- autoMove.sh
+++ autoMove.sh
@@ -5,28 +5,38 @@
 
 #test number of inputs from user
 #if no inputs, then explain the use of automove.sh
-#if 2 inputs, assign input to variables
+#if 2 inputs, assign input to variables and copy files to second location
+#on the same machine
+#if 4 inputs, assign input to variables and copy files to second locatio on a different machine on the same network
 if [ $# -eq 0 ]; then
+	#inform users of proper usage when 
 	echo "Usage: $0 targetDirectory destinationDirectory"
-	echo "targetDirectory is the directory you want to copy from"
-	echo "destinationDirectory is the directory you want to copy to"
+	echo "Usage: $0 targetDirectory UsernameAtHostMachine DestinationIP Destinationdirectory"
 	exit 1
 elif [ $# -eq 2 ]; then
 	dir1=$1
 	dest=$2
-fi
 
-#makes sure that the two directories are different
-if [ $dir1 -eq $dest ]
-	echo "Target and destination must be different"
-	exit 1
-fi
+	if [ $dir1 -eq $dest ]; then
+		echo "Target and destination must be different"
+		exit 1
+	fi	
 
-#sets up 'listener' on target directory
-#if a new file is found in the directory
-#copies file to target directory
-inotifywait -m $dir1 -e create -e moved_to |
+	inotifywait -m $dir1 -e create -e moved_to |
 	while read path action file; do
 		cp $dir1/$file $dest/$file
 	done
+elif [ $# -eq 4 ]; then
+	dir1=$1
+	username=$2
+	destIP=$3
+	dest=$4
+
+	inotifywait -m $dir1 -e create -e moved_to |
+	while read path action file; do
+		scp $dir1/$file $username@$destIP:$dest
+	done
+	
+fi
+
 
